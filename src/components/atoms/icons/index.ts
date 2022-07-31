982593a7215ec7bf2193dbf1b3aca0fb31520781
Krankenkopf <petricowork@gmail.com>
2022-07-31 14:26:33 +0300

    assets: add ui icons

---
+++ src/components/atoms/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as UserIcon } from './user';
+export { default as EnvelopeIcon } from './envelope';
+export { default as CheckIcon } from './check';
+export { default as CalendarIcon } from './calendar';
+export { default as CircleIcon } from './circle';
+export { default as ExclamationIcon } from './exclamation';
+export { default as PhoneIcon } from './phone';
