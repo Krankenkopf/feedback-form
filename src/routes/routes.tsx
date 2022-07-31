df293bba16b3caeaa2d54b8b5710c4968128e5db
Krankenkopf <petricowork@gmail.com>
2022-07-31 14:45:53 +0300

    hotfix: fix eslint rule & routing

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,7 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 
 const AppRoutes = () => (
   <Routes>
-    <Route path="/" element={<HomePage />} />
+    <Route path="*" element={<HomePage />} />
   </Routes>
 );
 
